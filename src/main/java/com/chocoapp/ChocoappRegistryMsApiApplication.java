d36c7b706a2cd3ae06e7674a8097c19de160a415
Arun S <101079723+arun-gits@users.noreply.github.com>
2022-05-05 16:56:18 +0530

    added Dockerfile

--- src/main/java/com/chocoapp/ChocoappRegistryMsApiApplication.java
+++ src/main/java/com/chocoapp/ChocoappRegistryMsApiApplication.java
@@ -2,7 +2,8 @@ package com.chocoapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+@EnableEurekaServer
 @SpringBootApplication
 public class ChocoappRegistryMsApiApplication {
 
