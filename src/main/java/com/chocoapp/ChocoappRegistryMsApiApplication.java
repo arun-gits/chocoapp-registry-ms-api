4063b6feb712b056f2a9f8554f7a7a203eba8b66
Arun S <101079723+arun-gits@users.noreply.github.com>
2022-05-05 16:40:20 +0530

    added src

---
+++ src/main/java/com/chocoapp/ChocoappRegistryMsApiApplication.java
@@ -0,0 +1,13 @@
+package com.chocoapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChocoappRegistryMsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChocoappRegistryMsApiApplication.class, args);
+	}
+
+}
